6127fdd0f0940c5a4fea380448bb9409ce4d2a30
Samanta Scavassa <samanta.camargoterra@gmail.com>
2020-05-17 19:31:30 -0300

    Change package structure, added lombok, added http status

--- src/main/java/com/codenation/centraldeerros/CentralDeErrosApplication.java
+++ src/main/java/com/codenation/centraldeerros/CentralDeErrosApplication.java
@@ -2,8 +2,12 @@ package com.codenation.centraldeerros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.codenation.centraldeerros")
+@EnableJpaRepositories("com.codenation.centraldeerros.repositories")
+@EntityScan("com.codenation.centraldeerros.entities")
 public class CentralDeErrosApplication {
 
 	public static void main(String[] args) {

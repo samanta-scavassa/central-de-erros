f7f494988346c0a925b064c91d1344b05fb1d865
Samanta Scavassa <samanta.camargoterra@gmail.com>
2020-05-27 21:23:01 -0300

    user corrections and findbyemail

--- src/main/java/com/codenation/centraldeerros/repositories/UserRepository.java
+++ src/main/java/com/codenation/centraldeerros/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.codenation.centraldeerros.repositories;
 import com.codenation.centraldeerros.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    public User findUserByEmail(String email);
 }
